5c13211d485781d3ecb13c336601e02c06556f86
Jesús Salazar Fenoy <salazarfenoy@gmail.com>
2020-01-10 23:56:08 +0100

    Clase Sesion.

--- src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/dominio/Sesion.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/dominio/Sesion.java
@@ -3,124 +3,169 @@ package org.iesalandalus.programacion.tutorias.mvc.modelo.dominio;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Sesion {
-private static final LocalTime HORA_COMIENZO_CLASES = LocalTime.of(16, 00);
-private static final LocalTime HORA_FIN_CLASES = LocalTime.of(22, 15);
-public static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-public static final DateTimeFormatter FORMATO_HORA = DateTimeFormatter.ofPattern("HH:mm");
-private LocalDate fecha;
-private LocalTime horaInicio, horaFin;
-private int minutosDuracion;
-private Tutoria tutoria;
+	private static final LocalTime HORA_COMIENZO_CLASES = LocalTime.of(16, 00);
+	private static final LocalTime HORA_FIN_CLASES = LocalTime.of(22, 15);
+	public static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+	public static final DateTimeFormatter FORMATO_HORA = DateTimeFormatter.ofPattern("HH:mm");
+	private LocalDate fecha;
+	private LocalTime horaInicio, horaFin;
+	private int minutosDuracion;
+	private Tutoria tutoria;
 
-public Sesion(Tutoria tutoria, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, int minutosDuracion){
-	setTutoria(tutoria);
-	setFecha(fecha);
-	setHoraInicio(horaInicio);
-	setHoraFin(horaFin);
-	setMinutosDuracion(minutosDuracion);
-}
-
-public Sesion(Sesion sesion) {
-	if (sesion==null) {
-		throw new NullPointerException("ERROR: No es posible copiar una sesión nula.");
+	public Sesion(Tutoria tutoria, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, int minutosDuracion) {
+		setTutoria(tutoria);
+		setFecha(fecha);
+		setHoraInicio(horaInicio);
+		setHoraFin(horaFin);
+		setMinutosDuracion(minutosDuracion);
 	}
-	
-	setTutoria(sesion.tutoria);
-	setFecha(sesion.fecha);
-	setHoraInicio(sesion.horaInicio);
-	setHoraFin(sesion.horaFin);
-	setMinutosDuracion(sesion.minutosDuracion);
-	
-}
 
-private void comprobarValidezSesion() {
-	
-}
+	public Sesion(Sesion sesion) {
+		if (sesion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una sesión nula.");
+		}
 
-public static Sesion getSesionFicticia(Tutoria tutoria, LocalDate fecha) {
-	return null;
-	
-}
-public LocalDate getFecha() {
-	return fecha;
-}
+		setTutoria(sesion.tutoria);
+		setFecha(sesion.fecha);
+		setHoraInicio(sesion.horaInicio);
+		setHoraFin(sesion.horaFin);
+		setMinutosDuracion(sesion.minutosDuracion);
 
-private void setFecha(LocalDate fecha) {
-	this.fecha = fecha;
-}
+	}
 
-public LocalTime getHoraInicio() {
-	return horaInicio;
-}
+	private void comprobarValidezSesion() {
 
-private void setHoraInicio(LocalTime horaInicio) {
-	this.horaInicio = horaInicio;
-}
+	}
 
-public LocalTime getHoraFin() {
-	return horaFin;
-}
+	public static Sesion getSesionFicticia(Tutoria tutoria, LocalDate fecha) {
 
-private void setHoraFin(LocalTime horaFin) {
-	this.horaFin = horaFin;
-}
+		return new Sesion(tutoria, fecha, LocalTime.of(19, 00), LocalTime.of(22, 00), 30);
 
-public int getMinutosDuracion() {
-	return minutosDuracion;
-}
+	}
 
-private void setMinutosDuracion(int minutosDuracion) {
-	this.minutosDuracion = minutosDuracion;
-}
+	public LocalDate getFecha() {
+		return fecha;
+	}
 
-public Tutoria getTutoria() {
-	return tutoria;
-}
+	private void setFecha(LocalDate fecha) {
 
-private void setTutoria(Tutoria tutoria) {
-	tutoria = new Tutoria(tutoria);
-	this.tutoria = tutoria;
-}
+		if (fecha == null) {
+			throw new NullPointerException("ERROR: La fecha no puede ser nula.");
+		}
 
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
-	result = prime * result + ((tutoria == null) ? 0 : tutoria.hashCode());
-	return result;
-}
+		if (!fecha.isAfter(LocalDate.now())) {
+			throw new IllegalArgumentException("ERROR: Las sesiones de deben planificar para fechas futuras.");
+		}
+		this.fecha = fecha;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
+	public LocalTime getHoraInicio() {
+		return horaInicio;
+	}
+
+	private void setHoraInicio(LocalTime horaInicio) {
+
+		if (horaInicio == null) {
+			throw new NullPointerException("ERROR: La hora de inicio no puede ser nula.");
+		}
+		if (horaInicio.isAfter(HORA_FIN_CLASES) || horaInicio.isBefore(HORA_COMIENZO_CLASES)
+				|| horaInicio.equals(HORA_FIN_CLASES)) {
+			throw new IllegalArgumentException("ERROR: La hora de inicio no es válida.");
+		}
+
+		this.horaInicio = horaInicio;
+	}
+
+	public LocalTime getHoraFin() {
+		return horaFin;
+	}
+
+	private void setHoraFin(LocalTime horaFin) {
+
+		if (horaFin == null) {
+			throw new NullPointerException("ERROR: La hora de fin no puede ser nula.");
+		}
+
+		if (horaFin.isBefore(HORA_COMIENZO_CLASES) || horaFin.isAfter(HORA_FIN_CLASES)) {
+			throw new IllegalArgumentException("ERROR: La hora de fin no es válida.");
+		}
+
+		if (horaFin.equals(horaInicio) || horaFin.isBefore(horaInicio)) {
+			throw new IllegalArgumentException("ERROR: Las hora para establecer la sesión no son válidas.");
+		}
+
+		this.horaFin = horaFin;
+	}
+
+	public int getMinutosDuracion() {
+		return minutosDuracion;
+	}
+
+	private void setMinutosDuracion(int minutosDuracion) {
+		int minutosDuracionTotal = (int) horaInicio.until(horaFin, ChronoUnit.MINUTES);
+
+		if (minutosDuracion == 0) {
+			throw new IllegalArgumentException("ERROR: Los minutos de duración no son válidos.");
+		}
+		if (minutosDuracionTotal % minutosDuracion != 0) {
+			throw new IllegalArgumentException(
+					"ERROR: Los minutos de duración no es divisor de los minutos establecidos para toda la sesión.");
+		}
+		this.minutosDuracion = minutosDuracion;
+	}
+
+	public Tutoria getTutoria() {
+		return tutoria;
+	}
+
+	private void setTutoria(Tutoria tutoria) {
+
+		if (tutoria == null) {
+			throw new NullPointerException("ERROR: La tutoría no puede ser nula.");
+		}
+		tutoria = new Tutoria(tutoria);
+		this.tutoria = tutoria;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
+		result = prime * result + ((tutoria == null) ? 0 : tutoria.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sesion other = (Sesion) obj;
+		if (fecha == null) {
+			if (other.fecha != null)
+				return false;
+		} else if (!fecha.equals(other.fecha))
+			return false;
+		if (tutoria == null) {
+			if (other.tutoria != null)
+				return false;
+		} else if (!tutoria.equals(other.tutoria))
+			return false;
 		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Sesion other = (Sesion) obj;
-	if (fecha == null) {
-		if (other.fecha != null)
-			return false;
-	} else if (!fecha.equals(other.fecha))
-		return false;
-	if (tutoria == null) {
-		if (other.tutoria != null)
-			return false;
-	} else if (!tutoria.equals(other.tutoria))
-		return false;
-	return true;
-}
+	}
 
-@Override
-public String toString() {
-	return "Sesion [fecha=" + fecha + ", horaInicio=" + horaInicio + ", horaFin=" + horaFin + ", minutosDuracion="
-			+ minutosDuracion + ", tutoria=" + tutoria + "]";
-}
-
-
+	@Override
+	public String toString() {
+		String mensaje = String.format("tutoria=%s, fecha=%s, horaInicio=%s, horaFin=%s, minutosDuracion=%s",
+				getTutoria(), getFecha().format(FORMATO_FECHA), getHoraInicio(), getHoraFin(), getMinutosDuracion());
+		return mensaje;
+	}
 
 }
